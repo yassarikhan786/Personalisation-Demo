e830f7564ef65c1ba4f5547eb19ebd566a3a888c
Yassar <yassar.ilyas.khan@gmail.com>
2015-03-09 21:36:33 +0000

    JQuery Update
    Cleaned up JQuery.

--- Personalisation-Demo.js
+++ Personalisation-Demo.js
@@ -2,13 +2,20 @@ var line1LastPosition = 0;
 var line2LastPosition = 0;
 var line3LastPosition = 0;
 
+var totalLines = 3;
+
 var charWidth = 34;
 var charHeight = 27;
 
 var offSet = 4;
 var maxChar = 24;
-var imgWidth = 300;
-var imgHeight = 150;
+var imgWidth = 0;
+var imgHeight = 0;
+
+function GetImageUrl(code) {
+    var imageUrl = 'https://ealite.s3.amazonaws.com/Developers/Images/Person-chr/' + String.fromCharCode(code).toUpperCase() + '.png';
+    return imageUrl;
+}
 
 function RemoveCharacter(textBox, lastPosition, containingClass, top) {
     $(containingClass + 'Char' + (lastPosition - 1)).remove();
@@ -17,34 +24,27 @@ function RemoveCharacter(textBox, lastPosition, containingClass, top) {
     ReDraw(textBox.val().substring(0, textBox.val().length - 1), (lastPosition - 1), 'false', containingClass, '', top)
 }
 
-function GetImageUrl(code) {
-    var imageUrl = 'https://ealite.s3.amazonaws.com/Developers/Images/Person-chr/' + String.fromCharCode(code).toUpperCase() + '.png';
-    return imageUrl;
-}
-
 function ReDraw(textBoxValue, lastPosition, firstCharacter, containingClass, code, top) {
     if (firstCharacter == 'true') {
         $(containingClass).append('<img class="' + containingClass.substring(1) + 'Char' + lastPosition + '" src="' + GetImageUrl(code) + '" alt="char" title="char" style="position:absolute;top:' + top + 'px;left:' + ((imgWidth - charWidth) / 2) + 'px;"/>');
     } 
     else {
-        var currentPosition = 0;
+        var charIndex = 0;
         var calculatedPosition = 0;
-        
-        //var textBoxValue = textBox.val() + String.fromCharCode(code).toUpperCase();       
-        
+
         var letters = jQuery.map((textBoxValue).split(''), function(e) {
             var char = e + '';
-            if (currentPosition == 0) {     
+            if (charIndex == 0) {     
                 calculatedPosition = ((imgWidth - (charWidth * (textBoxValue.length))) / 2);
               
-                $(containingClass).append('<img class="' + containingClass.substring(1) + 'Char' + currentPosition + '" src="https://ealite.s3.amazonaws.com/Developers/Images/Person-chr/' + char.toUpperCase() + '.png" alt="char" title="char" style="position:absolute;top:' + top + 'px;left:' + (calculatedPosition + offSet) + 'px;"/>');
+                $(containingClass).append('<img class="' + containingClass.substring(1) + 'Char' + charIndex + '" src="https://ealite.s3.amazonaws.com/Developers/Images/Person-chr/' + char.toUpperCase() + '.png" alt="char" title="char" style="position:absolute;top:' + top + 'px;left:' + (calculatedPosition + offSet) + 'px;"/>');
             }
             else {
                 calculatedPosition = calculatedPosition + charWidth;  
                 
-                $('<img class="' + containingClass.substring(1) + 'Char' + currentPosition + '" src="https://ealite.s3.amazonaws.com/Developers/Images/Person-chr/' + char.toUpperCase() + '.png" alt="char" title="char" style="position:absolute;top:' + top + 'px;left:' + (calculatedPosition + offSet) + 'px;"/>').insertAfter(containingClass + 'Char' + (currentPosition - 1));
+                $('<img class="' + containingClass.substring(1) + 'Char' + charIndex + '" src="https://ealite.s3.amazonaws.com/Developers/Images/Person-chr/' + char.toUpperCase() + '.png" alt="char" title="char" style="position:absolute;top:' + top + 'px;left:' + (calculatedPosition + offSet) + 'px;"/>').insertAfter(containingClass + 'Char' + (charIndex - 1));
             }
-            currentPosition++;
+            charIndex++;
         });
     }
 }
@@ -52,7 +52,6 @@ function ReDraw(textBoxValue, lastPosition, firstCharacter, containingClass, cod
 function Update(textBox, lastPosition, containingClass, top, e) {
     if (lastPosition < maxChar) {   
         var code = e.keyCode;
-        var lastChar = $('.' + containingClass + '.char' + lastPosition);
         var firstCharacter = 'true';
         
         if (lastPosition > 0) 
@@ -67,50 +66,53 @@ function Update(textBox, lastPosition, containingClass, top, e) {
     }
 }
 
-$(document).ready(function () {
+$(document).ready(function () {   
+    var img = new Image();
+    img.onload = function() {
+      imgWidth = this.width;
+      imgHeight = this.height;
+    }
+    img.src = 'https://ealite.s3.amazonaws.com/Developers/Images/Person-bkg/BACKIMG3.jpg';
+    
     var textBoxLine1 = $('#tbLine1');
     var textBoxLine2 = $('#tbLine2');
     var textBoxLine3 = $('#tbLine3');
     
-    textBoxLine1.on('keypress', function (e) {
-        Update(textBoxLine1, line1LastPosition, '.line1', 12, e);
-        line1LastPosition++;
-    }).on('click keyup', function () {
+    textBoxLine1.on('click keyup keypress keydown', function () {
         var tempStr = $(this).val();
         $(this).focus().val(tempStr);
+    }).on('keypress', function (e) {
+        Update(textBoxLine1, textBoxLine1.val().length, '.line1', (((imgHeight / totalLines) * 0) + ((imgHeight / totalLines) - charHeight) / 2), e);
     }).on('keydown', function (e) {
         var code = e.keyCode;
-        if (code == 8) {
-            RemoveCharacter(textBoxLine1, line1LastPosition, '.line1', 12);
-            line1LastPosition--;
-           
+        if (code == 8 && textBoxLine1.val().length > 0) {
+            RemoveCharacter(textBoxLine1, textBoxLine1.val().length, '.line1', (((imgHeight / totalLines) * 0) + ((imgHeight / totalLines) - charHeight) / 2));
         }   
     });
     
-    textBoxLine2.on('keypress', function (e) {
-        Update(textBoxLine2, line2LastPosition, '.line2', 62, e);
+    textBoxLine2.on('click keyup keypress keydown', function () {
+        var tempStr = $(this).val();
+        $(this).focus().val(tempStr);
+    }).on('keypress', function (e) {
+        Update(textBoxLine2, textBoxLine2.val().length, '.line2', (((imgHeight / totalLines) * 1) + (((imgHeight / totalLines) - charHeight) / 2)), e);
         line2LastPosition++;
-    }).on('click keyup', function () {
-        var tempStr = $(this).val();
-        $(this).focus().val(tempStr);
     }).on('keydown', function (e) {
         var code = e.keyCode;
-        if (code == 8) {
-            RemoveCharacter(textBoxLine2, line2LastPosition, '.line2', 62);
-            line2LastPosition--;
+        if (code == 8 && textBoxLine2.val().length > 0) {
+            RemoveCharacter(textBoxLine2, textBoxLine2.val().length, '.line2', (((imgHeight / totalLines) * 1) + (((imgHeight / totalLines) - charHeight) / 2)));
         }   
     });
     
-    textBoxLine3.on('keypress', function (e) {
-        Update(textBoxLine3, line3LastPosition, '.line3', 112, e);
-        line3LastPosition++;
-    }).on('click keyup', function () {
+    textBoxLine3.on('click keyup keypress keydown', function () {
         var tempStr = $(this).val();
         $(this).focus().val(tempStr);
+    }).on('keypress', function (e) {
+        Update(textBoxLine3, textBoxLine3.val().length, '.line3', (((imgHeight / totalLines) * 2) + (((imgHeight / totalLines) - charHeight) / 2)), e);
+        line3LastPosition++;
     }).on('keydown', function (e) {
         var code = e.keyCode;
-        if (code == 8) {
-            RemoveCharacter(textBoxLine3, line3LastPosition, '.line3', 112);
+        if (code == 8 && textBoxLine3.val().length > 0) {
+            RemoveCharacter(textBoxLine3, textBoxLine3.val().length, '.line3', (((imgHeight / totalLines) * 2) + (((imgHeight / totalLines) - charHeight) / 2)));
             line3LastPosition--;
         }   
     });
